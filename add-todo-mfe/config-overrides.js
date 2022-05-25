39a8f6cb07480553f459f6649c9a49039129e42a
MIKHAILPC\Mikhail <mustonen94m@gmail.com>
2022-05-25 11:56:03 +0300

    3.2. Add build customization

---
+++ add-todo-mfe/config-overrides.js
@@ -0,0 +1,30 @@
+const { override, disableChunk, addWebpackExternals } = require('customize-cra');
+
+const BUNDLE_NAME = 'add-todo';
+
+function changeName(config) {
+  config.output = {
+    ...config.output,
+    filename: `static/js/${BUNDLE_NAME}.js`,
+  };
+
+  config.plugins.map((plugin, i) => {
+    if (plugin.options && plugin.options.filename && plugin.options.filename.includes('static/css')) {
+      config.plugins[i].options = {
+        ...config.plugins[i].options,
+        filename: `static/css/${BUNDLE_NAME}.css`,
+      };
+    }
+  });
+
+  return config;
+}
+
+module.exports = override(
+  changeName,
+  disableChunk(),
+  addWebpackExternals({
+    react: 'React',
+    'react-dom': 'ReactDOM',
+  }),
+);
--- add-todo-mfe/src/index.tsx
+++ add-todo-mfe/src/index.tsx
@@ -6,11 +6,27 @@ import './index.css';
 import App from './App';
 import { store } from './state';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-);
+class AddTodoMFE extends HTMLElement {
+  private root: ReactDOM.Root;
+
+  constructor() {
+    super();
+    this.root = ReactDOM.createRoot(this);
+  }
+
+  connectedCallback() {
+    this.root.render(
+      <React.StrictMode>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </React.StrictMode>,
+    );
+  }
+
+  disconnectedCallback() {
+    this.root.unmount();
+  }
+}
+
+customElements.define('add-todo', AddTodoMFE);
