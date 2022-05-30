caa1d5b3f1803313e23f9f124dfdf9c6974b1fb1
MIKHAILPC\Mikhail <mustonen94m@gmail.com>
2022-05-30 11:18:12 +0300

    Refactor available global actions

--- container/src/state/store.ts
+++ container/src/state/store.ts
@@ -1,22 +1,19 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
-import { createReduxHistoryContext } from 'redux-first-history';
+import { CALL_HISTORY_METHOD, createReduxHistoryContext, LOCATION_CHANGE } from 'redux-first-history';
 import { createBrowserHistory } from 'history';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { GlobalStore } from 'redux-micro-frontend';
 
 import { globalStore } from '../globalStoreUtils';
 
 const { createReduxHistory, routerMiddleware, routerReducer } = createReduxHistoryContext({
   history: createBrowserHistory(),
 });
-
 export const store = createStore(
   combineReducers({
     router: routerReducer,
   }),
   composeWithDevTools(applyMiddleware(routerMiddleware)),
 );
-
 export const history = createReduxHistory(store);
 
-globalStore.RegisterStore('Container', store, [GlobalStore.AllowAll]);
+globalStore.RegisterStore('Container', store, [LOCATION_CHANGE, CALL_HISTORY_METHOD]);
